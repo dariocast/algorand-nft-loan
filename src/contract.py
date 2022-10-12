d85d88f3dcfc0a3e7361d511773a4a384d9850d0
Dario Castellano <dario.castellano@etuitus.it>
2022-10-12 17:21:51 +0200

    refactor: struct simplified and sandbox test

--- src/contract.py
+++ src/contract.py
@@ -6,14 +6,13 @@ from beaker import *
 from typing import Final
 from algosdk.future import transaction
 
-
 # Create a class, subclassing Application from beaker
 from src.utils.client import get_algod_client
 
 
-class HelloBeaker(Application):
+class BorrowMyNFT(Application):
     nft_id: Final[ApplicationStateValue] = ApplicationStateValue(
-        stack_type=TealType.bytes,
+        stack_type=TealType.uint64,
         descr="A handle to retrieve the correct NFT",
     )
     borrower_address: Final[ApplicationStateValue] = ApplicationStateValue(
@@ -22,22 +21,28 @@ class HelloBeaker(Application):
     )
     lender_address: Final[ApplicationStateValue] = ApplicationStateValue(
         stack_type=TealType.bytes,
-        descr="Current bidder",
+        descr="Current highest bidder",
     )
-    n_algos: Final[ApplicationStateValue] = ApplicationStateValue(
+    highest_bid: Final[ApplicationStateValue] = ApplicationStateValue(
         stack_type=TealType.uint64,
+        default=Int(0),
         descr="Highest bid",
     )
     loan_threshold: Final[ApplicationStateValue] = ApplicationStateValue(
         stack_type=TealType.uint64,
-        descr="Auction staring amount",
+        default=Int(0),
+        descr="Auction starting amount",
     )
     auction_period: Final[ApplicationStateValue] = ApplicationStateValue(
         stack_type=TealType.uint64,
+        # 1 day = 86400 seconds, 1 block = 5 seconds, 1 day = 17280 blocks
+        default=Int(17280),
         descr="Number of blocks from the offer creation",
     )
     payback_deadline: Final[ApplicationStateValue] = ApplicationStateValue(
         stack_type=TealType.uint64,
+        # 1 day = 86400 seconds, 1 block = 5 seconds, 1 day = 17280 blocks
+        default=Int(17280),
         descr="Number of blocks from the bid acceptance",
     )
     last_interest_update_block: Final[ApplicationStateValue] = ApplicationStateValue(
@@ -47,12 +52,43 @@ class HelloBeaker(Application):
     )
     debt_left: Final[ApplicationStateValue] = ApplicationStateValue(
         stack_type=TealType.uint64,
+        default=Int(0),
         descr="The current debt. debt_left=debt_left*((1+interset_rate)^(current_block - last_interest_update_block",
     )
 
+    # Contract address minimum balance
+    MIN_BAL = Int(100000)
+
+    # Algorand minimum txn fee
+    FEE = Int(1000)
+
     @create
     def create(self):
+        """Deploys the contract and intialize app states"""
         return self.initialize_application_state()
+
+    @opt_in
+    def opt_in(self):
+        # No local state to set, so approve
+        return Approve()
+
+    @close_out
+    def close_out(self):
+        return Approve()
+
+    @delete(authorize=Authorize.only(Global.creator_address()))
+    def delete(self):
+        return Approve()
+
+    # Add an external method with ABI method signature `hello(string)string`
+    @external
+    def health(self, output: abi.String):
+        # Set output to the result of `Hello, `+name
+        return output.set(
+            Bytes(
+                f"Contract {Global.current_application_id()} is up and running"
+            )
+        )
 
     @external(authorize=Authorize.only(Global.creator_address()))
     def PayMe(self, receiver: abi.Account):
@@ -64,10 +100,10 @@ class HelloBeaker(Application):
             TxnField.fee: Global.min_txn_fee()
         })
 
-    @external
-    def Offer (nft: abi.String, loan_threshold: abi.Uint64, auction_period:abi.Uint64, payback_deadline:abi.Uint64):
-        # borrower_address=tx.sender; nft=?????
-        pass
+    # @external
+    # def Offer(nft: abi.String, loan_threshold: abi.Uint64, auction_period: abi.Uint64, payback_deadline: abi.Uint64):
+    #     # borrower_address=tx.sender; nft=?????
+    #     pass
 
     # @external
     # def Bid (n_algos: abi.Uint64):
@@ -95,22 +131,20 @@ class HelloBeaker(Application):
     # def LoanExpires ():
     # pass
 
-    # Add an external method with ABI method signature `hello(string)string`
-    @external
-    def hello(self, name: abi.String, *, output: abi.String):
-        # Set output to the result of `Hello, `+name
-        return output.set(Concat(Bytes("Hello, "), name.get()))
-
 
 def demo():
     # Create an Application client
+    all_sandbox_accounts = sandbox.get_accounts()
+    contract_owner = all_sandbox_accounts.pop()
+    borrower = all_sandbox_accounts.pop()
+    lender = all_sandbox_accounts.pop()
 
     acc = sandbox.get_accounts().pop()
     app_client = client.ApplicationClient(
         # Get sandbox algod client
         client=sandbox.get_algod_client(),
         # Instantiate app with the program version (default is MAX_TEAL_VERSION)
-        app=HelloBeaker(version=6),
+        app=BorrowMyNFT(version=6),
         # Get acct from sandbox and pass the signer
         signer=acc.signer  # pop().signer,
     )
@@ -126,7 +160,7 @@ def demo():
     print(app_client.client.account_info(acc.address).get('amount'))
     app_client.fund(5 * consts.algo)
     print(app_client.client.account_info(acc.address).get('amount'))
-    result = app_client.call(HelloBeaker.PayMe, receiver=acc.address)
+    result = app_client.call(BorrowMyNFT.PayMe, receiver=acc.address)
     print(result.return_value, result.decode_error)
     print((app_client.client.account_info(acc.address)).get('amount'))
     print(acc.address)
--- src/tests/accounts_test.py
+++ src/tests/accounts_test.py
@@ -1,12 +1,29 @@
+from beaker import sandbox
+
 from src.utils.accounts import *
 from src.utils.client import *
 
 
-def main():
+def internal_accounts():
     private_key, my_address = get_default_algorand_keypair()
     algod_client = get_algod_client(private_key, my_address)
     account_info = check_balance(algod_client, my_address)
 
+def sandbox_accounts():
+    # get accounts from sandbox
+    accts = sandbox.get_accounts()
+    # print accts info
+    print(*accts, sep="\n", end="\n\n")
+    index = 0
+    for acct in accts:
+        print(f"Account {index}:")
+        print(f"Address: {acct.address}")
+        print(f"Private key: {acct.private_key}")
+        print(f"Signer: {acct.signer}")
+        info = sandbox.get_algod_client().account_info(acct.address)
+        print(f"Balance is {info.get('amount')}\n")
+        index += 1
+
 
 if __name__ == "__main__":
-    main()
+    sandbox_accounts()
